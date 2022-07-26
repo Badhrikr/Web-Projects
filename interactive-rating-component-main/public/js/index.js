436310ebb9538b6ae6d0b109bde443e225f6eb20
Badhrikr <badhrimkg@gmail.com>
2022-07-26 15:18:31 +0530

    js update

--- interactive-rating-component-main/public/js/index.js
+++ interactive-rating-component-main/public/js/index.js
@@ -1,21 +1,17 @@
+const wrapper = document.querySelector(".wrapper")
+const ratings = document.querySelector(".ratings")
 const rating = document.querySelectorAll(".card__btn")
 const submitBtn = document.querySelector(".subbtn")
-const result = document.querySelector("body")
+const result = document.querySelector(".rate")
 
 for(let i=0;i < rating.length;i++){
   rating[i].addEventListener('click',function(){
     console.log(rating[i].value)
 
     submitBtn.addEventListener('click', function(){
-      result.innerHTML = `<div class="container ratings">
-      <img src="./assets/images/illustration-thank-you.svg" alt="A Star logo">
-      <div class="ratings__result">You selected ${rating[i].textContent}  out of 5</div>
-      <div class="card__content ratings__content">
-        <h1 class="card__heading ratings__heading">Thank you!</h1>
-        <p class="card__description ratings__description">We appreciate you taking the time to give a rating. If you ever need more support, 
-          don’t hesitate to get in touch!</p>      
-      </div>
-    </div>`
+      result.textContent = rating[i].value;
+      ratings.style.display = "flex"
+      wrapper.style.display = "none"
     })
   })
 }
